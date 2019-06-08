196c64ff89cfeddd40b4e0ed7a1c51b374c28bee
Sone Gillis <sone_gillis@yahoo.com>
2019-06-08 13:40:38 +0100

    Immplement the my_any?, my_none? and my_count methods

--- enumerable.rb
+++ enumerable.rb
@@ -1,35 +1,57 @@
 module Enumerable
-    def my_each(array)
-        for e in array
+    def my_each
+        for e in self
             yield(e)
         end
     end
-    def my_each_with_index(array)
+    def my_each_with_index
         for i in (0...array.length)
             yield array[i], i
         end
     end
-    def my_select(array)
+    def my_select
         new_array = []
-        my_each(array){ |n| new_array.push(n) if yield(n) }
+        self.my_each{ |n| new_array.push(n) if yield(n) }
         new_array
     end
-    def my_all?(array)
+    def my_all?
         if block_given?
-            my_each(array){ |n| return false if !yield(n) }
-            
+            self.my_each{ |n| return false if !yield(n) }
         else
-            my_each(array){ |n| return false if !n }
+            self.my_each{ |n| return false if !n }
         end
         return true
     end
+    def my_any?
+        if block_given?
+            self.my_each{ |n| return true if yield(n) }
+        else
+            self.my_each{ |n| return true if n }
+        end
+        return false
+    end
+    def my_none?
+        if block_given?
+            self.my_each{ |n| return false if yield(n) }
+        else
+            self.my_each{ |n| return false if n }
+        end
+        return true
+    end
+    def my_count(obj=nil)
+        count = 0
+        if block_given?
+            self.my_each{ |n| count += 1 if yield(n) }
+        else
+            if obj.nil?
+                self.my_each{ |n| count += 1 }
+            else
+                self.my_each{ |n| count += 1 if obj == n }
+            end
+        end
+        return count
+    end
 end
 
-class Test
-    include Enumerable
+puts [3,4,5,2].my_count(5) do
 end
-test = Test.new
-
-puts test.my_all?([2,4]){|n|
-    n % 2 == 0
-}
